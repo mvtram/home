1aeea424fee8b6a7e52c38eb075b4aa7aa538d55
mvtram <mvenkat0897@gmail.com>
2022-07-16 15:14:44 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.4fb7883d328c6cf50116d0fe96ef9a95.js"
 );
 
 self.addEventListener('message', (event) => {
